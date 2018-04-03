654f31a6fcd75583cd5329f6c3c350d8572eb980
aycabta <aycabta@gmail.com>
2018-04-03 19:27:41 +0900

    Fix usage of #inject

--- lib/reline/line_editor.rb
+++ lib/reline/line_editor.rb
@@ -79,7 +79,7 @@ class Reline::LineEditor
   end
 
   private def calculate_width(str)
-    str.grapheme_clusters.inject(0) { |width, gc| width += Reline::Unicode.get_mbchar_width(gc) }
+    str.grapheme_clusters.inject(0) { |width, gc| width + Reline::Unicode.get_mbchar_width(gc) }
   end
 
   private def edit_insert(key)
