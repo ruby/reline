3e338037c788cb2b795eedb12ecd11d030832b12
aycabta <aycabta@gmail.com>
2018-05-13 23:44:34 +0900

    Define input_keys method for test

--- test/key_actor_emacs_test.rb
+++ test/key_actor_emacs_test.rb
@@ -6,82 +6,60 @@ class Reline::KeyActor::Emacs::Test < Test::Unit::TestCase
     @line_editor = Reline::LineEditor.new(Reline::KeyActor::Emacs, @prompt)
   end
 
+  def input_keys(input)
+    input.bytes.each do |byte|
+      @line_editor.input_key(byte)
+    end
+  end
+
   def test_ed_insert_one
-    @line_editor.input_key('a'.ord)
+    input_keys('a')
     assert_equal(@line_editor.line, 'a')
   end
 
   def test_ed_insert_two
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
+    input_keys('ab')
     assert_equal(@line_editor.line, 'ab')
   end
 
   def test_move_next_and_prev
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key('d'.ord)
-    @line_editor.input_key("\C-b".ord)
-    @line_editor.input_key("\C-b".ord)
-    @line_editor.input_key("\C-f".ord)
-    @line_editor.input_key('c'.ord)
+    input_keys("abd\C-b\C-b\C-fc")
     assert_equal(@line_editor.line, 'abcd')
   end
 
   def test_move_to_beg_end
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key('c'.ord)
-    @line_editor.input_key('d'.ord)
-    @line_editor.input_key("\C-a".ord)
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key("\C-e".ord)
-    @line_editor.input_key('e'.ord)
+    input_keys("bcd\C-aa\C-ee")
     assert_equal(@line_editor.line, 'abcde')
   end
 
   def test_newline
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key("\C-m".ord)
+    input_keys("ab\C-m")
     assert_equal(@line_editor.line, "ab\n")
     assert(@line_editor.finished?)
   end
 
   def test_delete_prev_char
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key("\C-h".ord)
+    input_keys("ab\C-h")
     assert_equal(@line_editor.line, 'a')
   end
 
   def test_ed_kill_line
-    @line_editor.input_key("\C-k".ord)
+    input_keys("\C-k")
     assert_equal(@line_editor.line, '')
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key('c'.ord)
-    @line_editor.input_key("\C-k".ord)
+    input_keys("abc\C-k")
     assert_equal(@line_editor.line, 'abc')
-    @line_editor.input_key("\C-b".ord)
-    @line_editor.input_key("\C-k".ord)
+    input_keys("\C-b\C-k")
     assert_equal(@line_editor.line, 'ab')
   end
 
   def test_em_kill_line
-    @line_editor.input_key("\C-u".ord)
+    input_keys("\C-u")
     assert_equal(@line_editor.line, '')
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key('c'.ord)
-    @line_editor.input_key("\C-u".ord)
+    input_keys("abc\C-u")
     assert_equal(@line_editor.line, '')
-    @line_editor.input_key('a'.ord)
-    @line_editor.input_key('b'.ord)
-    @line_editor.input_key('c'.ord)
-    @line_editor.input_key("\C-b".ord)
-    @line_editor.input_key("\C-u".ord)
+    input_keys("abc\C-b\C-u")
     assert_equal(@line_editor.line, 'c')
-    @line_editor.input_key("\C-u".ord)
+    input_keys("\C-u")
     assert_equal(@line_editor.line, 'c')
   end
 end
