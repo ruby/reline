9ecdcfcf3e2da12742359310391471562148bb77
aycabta <aycabta@gmail.com>
2018-05-19 21:51:25 +0900

    Fix expected arg position

--- test/key_actor_emacs_test.rb
+++ test/key_actor_emacs_test.rb
@@ -14,126 +14,126 @@ class Reline::KeyActor::Emacs::Test < Test::Unit::TestCase
 
   def test_ed_insert_one
     input_keys('a')
-    assert_equal(@line_editor.line, 'a')
+    assert_equal('a', @line_editor.line)
   end
 
   def test_ed_insert_two
     input_keys('ab')
-    assert_equal(@line_editor.line, 'ab')
+    assert_equal('ab', @line_editor.line)
   end
 
   def test_ed_insert_mbchar_one
     input_keys('か')
-    assert_equal(@line_editor.line, 'か')
+    assert_equal('か', @line_editor.line)
   end
 
   def test_ed_insert_mbchar_two
     input_keys('かき')
-    assert_equal(@line_editor.line, 'かき')
+    assert_equal('かき', @line_editor.line)
   end
 
   def test_ed_insert_grapheme_cluster_by_plural_code_points
     input_keys("か\u3099")
-    assert_equal(@line_editor.line, "か\u3099")
+    assert_equal("か\u3099", @line_editor.line)
   end
 
   def test_ed_insert_plural_grapheme_clusters_by_plural_code_points
     input_keys("か\u3099き\u3099")
-    assert_equal(@line_editor.line, "か\u3099き\u3099")
+    assert_equal("か\u3099き\u3099", @line_editor.line)
   end
 
   def test_move_next_and_prev
     input_keys("abd\C-b\C-b\C-fc")
-    assert_equal(@line_editor.line, 'abcd')
+    assert_equal('abcd', @line_editor.line)
   end
 
   def test_move_next_and_prev_for_mbchar
     input_keys("かきけ\C-b\C-b\C-fく")
-    assert_equal(@line_editor.line, 'かきくけ')
+    assert_equal('かきくけ', @line_editor.line)
   end
 
   def test_move_next_and_prev_for_grapheme_clusters_by_plural_code_points
     input_keys("か\u3099き\u3099け\u3099\C-b\C-b\C-fく\u3099")
-    assert_equal(@line_editor.line, "か\u3099き\u3099く\u3099け\u3099")
+    assert_equal("か\u3099き\u3099く\u3099け\u3099", @line_editor.line)
   end
 
   def test_move_to_beg_end
     input_keys("bcd\C-aa\C-ee")
-    assert_equal(@line_editor.line, 'abcde')
+    assert_equal('abcde', @line_editor.line)
   end
 
   def test_newline
     input_keys("ab\C-m")
-    assert_equal(@line_editor.line, "ab\n")
+    assert_equal("ab\n", @line_editor.line)
     assert(@line_editor.finished?)
   end
 
   def test_delete_prev_char
     input_keys("ab\C-h")
-    assert_equal(@line_editor.line, 'a')
+    assert_equal('a', @line_editor.line)
   end
 
   def test_delete_prev_char_for_mbchar
     input_keys("かき\C-h")
-    assert_equal(@line_editor.line, 'か')
+    assert_equal('か', @line_editor.line)
   end
 
   def test_delete_prev_char_for_grapheme_clusters_by_plural_code_points
     input_keys("か\u3099き\u3099\C-h")
-    assert_equal(@line_editor.line, "か\u3099")
+    assert_equal("か\u3099", @line_editor.line)
   end
 
   def test_ed_kill_line
     input_keys("\C-k")
-    assert_equal(@line_editor.line, '')
+    assert_equal('', @line_editor.line)
     input_keys("abc\C-k")
-    assert_equal(@line_editor.line, 'abc')
+    assert_equal('abc', @line_editor.line)
     input_keys("\C-b\C-k")
-    assert_equal(@line_editor.line, 'ab')
+    assert_equal('ab', @line_editor.line)
   end
 
   def test_em_kill_line
     input_keys("\C-u")
-    assert_equal(@line_editor.line, '')
+    assert_equal('', @line_editor.line)
     input_keys("abc\C-u")
-    assert_equal(@line_editor.line, '')
+    assert_equal('', @line_editor.line)
     input_keys("abc\C-b\C-u")
-    assert_equal(@line_editor.line, 'c')
+    assert_equal('c', @line_editor.line)
     input_keys("\C-u")
-    assert_equal(@line_editor.line, 'c')
+    assert_equal('c', @line_editor.line)
   end
 
   def test_ed_move_to_beg
     input_keys("abd\C-bc\C-a012")
-    assert_equal(@line_editor.line, '012abcd')
+    assert_equal('012abcd', @line_editor.line)
     input_keys("\C-aABC")
-    assert_equal(@line_editor.line, 'ABC012abcd')
+    assert_equal('ABC012abcd', @line_editor.line)
     input_keys("\C-f" * 10 + "\C-aa")
-    assert_equal(@line_editor.line, 'aABC012abcd')
+    assert_equal('aABC012abcd', @line_editor.line)
   end
 
   def test_ed_move_to_end
     input_keys("abd\C-bc\C-e012")
-    assert_equal(@line_editor.line, 'abcd012')
+    assert_equal('abcd012', @line_editor.line)
     input_keys("\C-eABC")
-    assert_equal(@line_editor.line, 'abcd012ABC')
+    assert_equal('abcd012ABC', @line_editor.line)
     input_keys("\C-b" * 10 + "\C-ea")
-    assert_equal(@line_editor.line, 'abcd012ABCa')
+    assert_equal('abcd012ABCa', @line_editor.line)
   end
 
   def test_em_delete_or_list
     input_keys("ab\C-a\C-d")
-    assert_equal(@line_editor.line, 'b')
+    assert_equal('b', @line_editor.line)
   end
 
   def test_em_delete_or_list_for_mbchar
     input_keys("かき\C-a\C-d")
-    assert_equal(@line_editor.line, 'き')
+    assert_equal('き', @line_editor.line)
   end
 
   def test_em_delete_or_list_for_grapheme_clusters_by_plural_code_points
     input_keys("か\u3099き\u3099\C-a\C-d")
-    assert_equal(@line_editor.line, "き\u3099")
+    assert_equal("き\u3099", @line_editor.line)
   end
 
   def test_ed_clear_screen
