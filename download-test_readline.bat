d23d466229887c33b74cf47fa8bd1a9b006b558d
manga_osyo <manga.osyo@gmail.com>
2019-08-26 00:00:39 +0900

    Add with CD.

--- download-test_readline.bat
+++ download-test_readline.bat
@@ -1,28 +1,28 @@
 @echo off
 echo "-- Download test/readline --"
 mkdir test\ext\readline
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/test/readline/helper.rb test\ext\readline\helper.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/test/readline/test_readline.rb test\ext\readline\test_readline.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/test/readline/test_readline_history.rb test\ext\readline\test_readline_history.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/test/readline/helper.rb %CD%\test\ext\readline\helper.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/test/readline/test_readline.rb %CD%\test\ext\readline\test_readline.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/test/readline/test_readline_history.rb %CD%\test\ext\readline\test_readline_history.rb
 
 mkdir tool
 bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/colorize.rb tool
 
 mkdir tool\lib
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/leakchecker.rb tool\lib\leakchecker.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/envutil.rb tool\lib\envutil.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/colorize.rb tool\lib\colorize.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/find_executable.rb tool\lib\find_executable.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/leakchecker.rb %CD%\tool\lib\leakchecker.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/envutil.rb %CD%\tool\lib\envutil.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/colorize.rb %CD%\tool\lib\colorize.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/find_executable.rb %CD%\tool\lib\find_executable.rb
 
 mkdir tool\lib\unit\minitest
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/minitest/unit.rb tool\lib\minitest\unit.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/minitest/unit.rb %CD%\tool\lib\minitest\unit.rb
 
 mkdir tool\lib\unit\test
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit.rb tool\lib\test\unit.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/assertions.rb tool\lib\test\unit\assertions.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/core_assertions.rb tool\lib\test\unit\core_assertions.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/parallel.rb tool\lib\test\unit\parallel.rb
-bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/testcase.rb tool\lib\test\unit\testcase.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit.rb %CD%\tool\lib\test\unit.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/assertions.rb %CD%\tool\lib\test\unit\assertions.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/core_assertions.rb %CD%\tool\lib\test\unit\core_assertions.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/parallel.rb %CD%\tool\lib\test\unit\parallel.rb
+bitsadmin.exe /TRANSFER get_readline_test_file https://raw.githubusercontent.com/ruby/ruby/trunk/tool/lib/test/unit/testcase.rb %CD%\tool\lib\test\unit\testcase.rb
 
 echo "-- Finish download test/readline --"
 
