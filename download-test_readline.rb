5f3480c49ab810b9732b4bc9fb221439d4aaeee5
aycabta <aycabta@gmail.com>
2021-07-20 20:31:28 +0900

    Follow changing file path of ruby/ruby

--- download-test_readline.rb
+++ download-test_readline.rb
@@ -23,13 +23,13 @@ module HTTPS_DL
     ["test/readline"     , "test/ext/readline" , "test_readline.rb"         ],
     ["test/readline"     , "test/ext/readline" , "test_readline_history.rb" ],
     ["tool/lib"          , "tool/lib"          , "colorize.rb"              ],
+    ["tool/lib"          , "tool/lib"          , "core_assertions.rb"       ],
     ["tool/lib"          , "tool/lib"          , "envutil.rb"               ],
     ["tool/lib"          , "tool/lib"          , "find_executable.rb"       ],
     ["tool/lib"          , "tool/lib"          , "leakchecker.rb"           ],
     ["tool/lib/minitest" , "tool/lib/minitest" , "unit.rb"                  ],
     ["tool/lib/test"     , "tool/lib/test"     , "unit.rb"                  ],
     ["tool/lib/test/unit", "tool/lib/test/unit", "assertions.rb"            ],
-    ["tool/lib/test/unit", "tool/lib/test/unit", "core_assertions.rb"       ],
     ["tool/lib/test/unit", "tool/lib/test/unit", "parallel.rb"              ],
     ["tool/lib/test/unit", "tool/lib/test/unit", "testcase.rb" ]
   ]
