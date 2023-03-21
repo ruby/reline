4851d7be2e440ac6ffd52ad6e8546f783ae367d4
Phillip Hellewell <sshock@gmail.com>
2023-03-20 20:15:29 -0600

    Match behavior of readline 8.2
    In the latest readline (8.2), page-up and page-down are bound to
    history-search-backward and history-search-forward by default.
    
    We would like reline to have the same default behavior.

--- test/reline/test_ansi_with_terminfo.rb
+++ test/reline/test_ansi_with_terminfo.rb
@@ -35,14 +35,14 @@ class Reline::ANSI::TestWithTerminfo < Reline::TestCase
 
   # PgUp key
   def test_kpp
-    assert_key_binding(Reline::Terminfo.tigetstr('kpp'), :ed_ignore)
+    assert_key_binding(Reline::Terminfo.tigetstr('kpp'), :ed_search_prev_history)
   rescue Reline::Terminfo::TerminfoError => e
     omit e.message
   end
 
   # PgDn key
   def test_knp
-    assert_key_binding(Reline::Terminfo.tigetstr('knp'), :ed_ignore)
+    assert_key_binding(Reline::Terminfo.tigetstr('knp'), :ed_search_next_history)
   rescue Reline::Terminfo::TerminfoError => e
     omit e.message
   end
