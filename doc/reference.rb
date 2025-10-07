4f072e7b60755bb4baed689f3502dc8a2cd769e9
BurdetteLamar <burdettelamar@yahoo.com>
2025-10-07 02:05:44 +0100

    Reference maker

--- doc/reference.rb
+++ doc/reference.rb
@@ -2,8 +2,10 @@ require 'rexml/document'
 
 include REXML
 
-# CSS styles.
-Style = <<STYLE
+class ReferenceMaker
+
+  # CSS styles.
+  Style = <<STYLE
     table {
     }
     tr:nth-child(even) {
@@ -37,188 +39,192 @@ Style = <<STYLE
     }
 STYLE
 
-# Here's the data.
-Sections = {
-  'Commands for Moving' => [
-    %w[C-a beginning-of-line true true true true],
-    %w[C-e end-of-line  true true true true],
-    %w[C-f forward-char true true true true],
-    %w[C-b backward-char true true true true],
-    %w[M-f forward-word true true true true],
-    %w[M-b backward-word true true true true],
-    %w[M-C-l clear-display true true true true],
-    %w[C-l clear-screen true true true true],
-  ],
-  'Commands For Manipulating The History' => [
-    ['Newline or Return', 'accept-line', 'true'],
-    %w[C-p previous-history true],
-    %w[C-n next-history true],
-    %w[M-< beginning-of-history false],
-    %w[M-> end-of-history false],
-    %w[C-r reverse-search-history true],
-    %w[C-s forward-search-history false],
-    %w[M-p non-incremental-reverse-search-history true],
-    %w[M-n non-incremental-forward-search-history true],
-    %w[M-C-y yank-nth-arg false],
-    ['M-. or M-_', 'yank-last-arg', 'false'],
-    %w[C-o operate-and-get-next false],
-  ],
-  'Commands For Changing Text' => [
-    ['usually C-d', 'end-of-file', 'true'],
-    %w[C-d delete-char true],
-    %w[Rubout backward-delete-char true],
-    ['C-q or C-v', 'quoted-insert', 'false'],
-    %w[M-TAB tab-insert false],
-    ['a, b, A, 1, !, …', 'self-insert', 'true'],
-    %w[C-t transpose-chars true],
-    %w[M-t transpose-words true],
-    %w[M-u upcase-word true],
-    %w[M-l downcase-word true],
-    %w[M-c capitalize-word true],
-  ],
-  'Killing and Yanking' => [
-    %w[C-k kill-line true],
-    ['C-x Rubout', 'backward-kill-line', 'false'],
-    %w[C-u unix-line-discard true],
-    %w[M-d kill-word true],
-    %w[M-DEL backward-kill-word false],
-    %w[C-w unix-word-rubout true],
-    %w[C-y yank true],
-    %w[M-y yank-pop false],
-  ],
-  'Specifying Numeric Arguments' => [
-    ['M-0, M-1, … M--', 'digit-argument', 'true']
-  ],
-  'Letting Readline Type for You' => [
-    %w[TAB complete true],
-    %w[M-? possible-completions false],
-    %w[M-* insert-completions false],
-  ],
-  'Keyboard Macros' => [
-    ['C-x (', 'start-kbd-macro', 'false'],
-    ['C-x )', 'end-kbd-macro', 'false'],
-    ['C-x e', 'call-last-kbd-macro', 'false'],
-  ],
-  'Some Miscellaneous Commands' => [
-    ['C-x C-r', 're-read-init-file'],
-    %w[C-g abort false],
-    ['M-A, M-B, M-x, …', 'do-lowercase-version'],
-    %w[ESC prefix-meta true],
-    ['C-_ or C-x C-u', 'undo', 'true'],
-    %w[M-r revert-line false],
-    %w[M-~ tilde-expand false],
-    %w[C-@ set-mark false],
-    ['C-x C-x', 'exchange-point-and-mark', 'false'],
-    ['M-C-]', 'character-search-backward', 'false'],
-    %w[M-# insert-comment false],
-    %w[M-x execute-named-command false],
-    %w[C-e emacs-editing-mode false],
-    %w[M-C-j vi-editing-mode false],
-  ]
-}
+  # Here's the data.
+  Applications = %w[reline irb ri debug]
+  Sections = {
+    'Commands for Moving' => [
+      %w[C-a beginning-of-line true true true true],
+      %w[C-e end-of-line  true true true true],
+      %w[C-f forward-char true true true true],
+      %w[C-b backward-char true true true true],
+      %w[M-f forward-word true true true true],
+      %w[M-b backward-word true true true true],
+      %w[M-C-l clear-display true true true true],
+      %w[C-l clear-screen true true true true],
+    ],
+    'Commands For Manipulating The History' => [
+      ['Newline or Return', 'accept-line', 'true'],
+      %w[C-p previous-history true],
+      %w[C-n next-history true],
+      %w[M-< beginning-of-history false],
+      %w[M-> end-of-history false],
+      %w[C-r reverse-search-history true],
+      %w[C-s forward-search-history false],
+      %w[M-p non-incremental-reverse-search-history true],
+      %w[M-n non-incremental-forward-search-history true],
+      %w[M-C-y yank-nth-arg false],
+      ['M-. or M-_', 'yank-last-arg', 'false'],
+      %w[C-o operate-and-get-next false],
+    ],
+    'Commands For Changing Text' => [
+      ['usually C-d', 'end-of-file', 'true'],
+      %w[C-d delete-char true],
+      %w[Rubout backward-delete-char true],
+      ['C-q or C-v', 'quoted-insert', 'false'],
+      %w[M-TAB tab-insert false],
+      ['a, b, A, 1, !, …', 'self-insert', 'true'],
+      %w[C-t transpose-chars true],
+      %w[M-t transpose-words true],
+      %w[M-u upcase-word true],
+      %w[M-l downcase-word true],
+      %w[M-c capitalize-word true],
+    ],
+    'Killing and Yanking' => [
+      %w[C-k kill-line true],
+      ['C-x Rubout', 'backward-kill-line', 'false'],
+      %w[C-u unix-line-discard true],
+      %w[M-d kill-word true],
+      %w[M-DEL backward-kill-word false],
+      %w[C-w unix-word-rubout true],
+      %w[C-y yank true],
+      %w[M-y yank-pop false],
+    ],
+    'Specifying Numeric Arguments' => [
+      ['M-0, M-1, … M--', 'digit-argument', 'true']
+    ],
+    'Letting Readline Type for You' => [
+      %w[TAB complete true],
+      %w[M-? possible-completions false],
+      %w[M-* insert-completions false],
+    ],
+    'Keyboard Macros' => [
+      ['C-x (', 'start-kbd-macro', 'false'],
+      ['C-x )', 'end-kbd-macro', 'false'],
+      ['C-x e', 'call-last-kbd-macro', 'false'],
+    ],
+    'Some Miscellaneous Commands' => [
+      ['C-x C-r', 're-read-init-file'],
+      %w[C-g abort false],
+      ['M-A, M-B, M-x, …', 'do-lowercase-version'],
+      %w[ESC prefix-meta true],
+      ['C-_ or C-x C-u', 'undo', 'true'],
+      %w[M-r revert-line false],
+      %w[M-~ tilde-expand false],
+      %w[C-@ set-mark false],
+      ['C-x C-x', 'exchange-point-and-mark', 'false'],
+      ['M-C-]', 'character-search-backward', 'false'],
+      %w[M-# insert-comment false],
+      %w[M-x execute-named-command false],
+      %w[C-e emacs-editing-mode false],
+      %w[M-C-j vi-editing-mode false],
+    ]
+  }
 
-# Make a TD element to show whether supported in the app.
-def td_for_support(value)
-  td = Element.new('td')
-  case value
-  when 'true'
-    td.add_attribute('id', 'supported')
-    check = "\u2714".encode('utf-8')
-    td.text = Text.new(check, false, nil, true)
-  when 'false'
-    td.add_attribute('id', 'unsupported')
-    cross = "\u2716".encode('utf-8')
-    td.text = Text.new(cross, false, nil, true)
-  when nil
-    td.add_attribute('id', 'support_unknown')
-    question_mark = '?'
-    td.text = Text.new(question_mark, false, nil, true)
-  else
-    raise value.to_s
+  # The order matters here.
+  Escapes = {
+    '_' => '_005f', # Must be first..
+    '-' => '_002d',
+    '(' => '_0028',
+    ')' => '_0029',
+    '<' => '_003c',
+    '>' => '_003e',
+    '.' => '_002e',
+    ',' => '_002c',
+    '…' => '_2026',
+    '?' => '_003f',
+    '*' => '_002a',
+    '!' => '_0021',
+    ']' => '_005d',
+    '~' => '_007e',
+    '#' => '_0023',
+    '@' => '_0040',
+    ' ' => '-' # Must be last.
+  }
+
+  # Make a TD element to show whether supported in the app.
+  def td_for_support(keys, application, supported_p)
+    td = Element.new('td')
+    case supported_p
+    when 'true'
+      td.add_attribute('id', 'supported')
+      check = "\u2714".encode('utf-8')
+      td.text = Text.new(check, false, nil, true)
+    when 'false'
+      td.add_attribute('id', 'unsupported')
+      cross = "\u2716".encode('utf-8')
+      td.text = Text.new(cross, false, nil, true)
+    when nil
+      td.add_attribute('id', 'support_unknown')
+      question_mark = '?'
+      td.text = Text.new(question_mark, false, nil, true)
+    else
+      raise value.to_s
+    end
+    td
   end
-  td
+
+  # Escapes, Readline's way.
+  def escape(keys, command)
+    s = "#{command} (#{keys})"
+    Escapes.each_pair do |old, new|
+      s.gsub!(old, new)
+    end
+    s
+  end
+
+  def initialize
+    doc = Document.new
+    doc.add_element(html = Element.new('html'))
+    html.add_element(head = Element.new('head'))
+    head.add_element(style = Element.new('style'))
+    style.text = Style
+    html.add_element(body = Element.new('body'))
+    Sections.each do |title, commands|
+      html.add_element(h2 = Element.new('h2'))
+      h2.text = title
+      html.add_element(table = Element.new('table'))
+      table.add_element(tr = Element.new('tr'))
+      %w[Keys Command].each do |heading|
+        tr.add_element(th = Element.new('th'))
+        th.add_attribute('rowspan', 2)
+        th.text = heading
+      end
+      tr.add_element(th = Element.new('th'))
+      th.add_attribute('colspan', 4)
+      th.text = 'Supported in Applications?'
+      table.add_element(tr = Element.new('tr'))
+      Applications.each do |heading|
+        tr.add_element(th = Element.new('th'))
+        th.add_attribute('width', '10%')
+        th.add_attribute('id', 'app_name')
+        th.text = heading
+      end
+      commands.each do |data|
+        keys = data.shift
+        command = data.shift
+        supported_p = data
+        table.add_element(tr = Element.new('tr'))
+        # Cell for Keys.
+        tr.add_element(td = Element.new('td'))
+        td.add_attribute('align', 'center')
+        td.add_element(code = Element.new('code'))
+        code.text = keys
+        # Cell for Command.
+        tr.add_element(td = Element.new('td'))
+        td.add_element(a = Element.new('a'))
+        href = 'https://tiswww.case.edu/php/chet/readline/readline.html#index-' +
+               escape(keys, command)
+        a.add_attribute('href', href)
+        a.add_element(code = Element.new('code'))
+        code.text = command
+        # Cells for app support.
+        Applications.each_with_index do |application, i|
+          tr.add_element(td_for_support(keys, application, supported_p[i]))
+        end
+      end
+    end
+    doc.write(indent: 2)
+  end
 end
 
-# The order matters here.
-Escapes = {
-  '_' => '_005f', # Must be first..
-  '-' => '_002d',
-  '(' => '_0028',
-  ')' => '_0029',
-  '<' => '_003c',
-  '>' => '_003e',
-  '.' => '_002e',
-  ',' => '_002c',
-  '…' => '_2026',
-  '?' => '_003f',
-  '*' => '_002a',
-  '!' => '_0021',
-  ']' => '_005d',
-  '~' => '_007e',
-  '#' => '_0023',
-  '@' => '_0040',
-  ' ' => '-' # Must be last.
-}
-
-# Escapes, Readline's way.
-def escape(keys, command)
-  s = "#{command} (#{keys})"
-  Escapes.each_pair do |old, new|
-    s.gsub!(old, new)
-  end
-  s
-end
-
-doc = Document.new
-doc.add_element(html = Element.new('html'))
-html.add_element(head = Element.new('head'))
-head.add_element(style = Element.new('style'))
-style.text = Style
-html.add_element(body = Element.new('body'))
-Sections.each do |title, commands|
-  html.add_element(h2 = Element.new('h2'))
-  h2.text = title
-  html.add_element(table = Element.new('table'))
-  table.add_element(tr = Element.new('tr'))
-  %w[Keys Command].each do |heading|
-    tr.add_element(th = Element.new('th'))
-    th.add_attribute('rowspan', 2)
-    th.text = heading
-  end
-  tr.add_element(th = Element.new('th'))
-  th.add_attribute('colspan', 4)
-  th.text = 'Supported in Applications?'
-  table.add_element(tr = Element.new('tr'))
-  %w[reline irb ri debug].each do |heading|
-    tr.add_element(th = Element.new('th'))
-    th.add_attribute('width', '10%')
-    th.add_attribute('id', 'app_name')
-    th.text = heading
-  end
-  commands.each do |data|
-    keys, command, reline, debug, irb, ri = data
-    table.add_element(tr = Element.new('tr'))
-    # Cell for Keys.
-    tr.add_element(td = Element.new('td'))
-    td.add_attribute('align', 'center')
-    td.add_element(code = Element.new('code'))
-    code.text = keys
-    # Cell for Command.
-    tr.add_element(td = Element.new('td'))
-    td.add_element(a = Element.new('a'))
-    href = 'https://tiswww.case.edu/php/chet/readline/readline.html#index-' +
-           escape(keys, command)
-    a.add_attribute('href', href)
-    a.add_element(code = Element.new('code'))
-    code.text = command
-    # Cells for app support.
-    tr.add_element(td_for_support(reline))
-    tr.add_element(td_for_support(debug))
-    tr.add_element(td_for_support(irb))
-    tr.add_element(td_for_support(ri))
-  end
-
-end
-
-doc.write(indent: 2)
-
+ReferenceMaker.new
